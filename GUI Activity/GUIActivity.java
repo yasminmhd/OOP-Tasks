43f437a4fb8e7b9411e5c6800f1f7b00c0612d24
yasminmhd <145632697+yasminmhd@users.noreply.github.com>
2024-11-14 22:19:48 +0800

    Create GUIActivity.java

---
+++ GUI Activity/GUIActivity.java
@@ -0,0 +1,10 @@
+package guiactivity;
+
+public class GUIActivity {
+
+    public static void main(String[] args) {
+        Project CoffeeFrame = new Project();
+        CoffeeFrame.setVisible(true);
+    }
+    
+}
