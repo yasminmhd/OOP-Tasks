792f0cc182b4889337b59bc3afe5b7b1bb7fdb07
yasminmhd <145632697+yasminmhd@users.noreply.github.com>
2024-11-14 23:00:29 +0800

    Add files via upload

---
+++ Chapter 4/SequenceNumbers.java
@@ -0,0 +1,14 @@
+package sequencenumbers;
+//class name SequenceNumbers
+public class SequenceNumbers {
+    public static void main(String[] args) {
+        
+        //outer loop initialization,condition,decrement
+        for(int i=9;i>=1;i--){
+            //inner loop
+            for(int j=2;j>=1;j--){
+                //print the numbers
+                System.out.print(i);
+            }System.out.print(" ");//print the spaces between the numbers
+        }
+    }}
