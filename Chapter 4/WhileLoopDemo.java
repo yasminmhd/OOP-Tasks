706aed9ebc730734ffeadf5562de8a63e1c47290
yasminmhd <145632697+yasminmhd@users.noreply.github.com>
2024-11-14 23:01:46 +0800

    Add files via upload

---
+++ Chapter 4/WhileLoopDemo.java
@@ -0,0 +1,9 @@
+package whileloopdemo;
+public class WhileLoopDemo {
+    public static void main(String[] args) {
+        int count=1;
+        System.out.println("Printing Numbers from 1 to 10");
+        while (count<=10){
+            System.out.println(count++);
+        }
+    }}
